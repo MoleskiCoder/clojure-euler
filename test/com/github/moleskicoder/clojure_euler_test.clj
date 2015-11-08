76b4a9d61626edc860a981fcb695d9709b70b087
Adrian Conlon <adrian.conlon@gmail.com>
2015-11-08 21:12:38 +0000

    Remove unused skeletal test.
    Signed-off-by: Adrian Conlon <adrian.conlon@gmail.com>

--- test/com/github/moleskicoder/clojure_euler_test.clj
+++
@@ -1,7 +0,0 @@
-(ns com.github.moleskicoder.clojure-euler-test
-  (:require [clojure.test :refer :all]
-            [com.github.moleskicoder.clojure-euler :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
