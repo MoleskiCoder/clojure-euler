4a5d6b1e2cf9364f3463fe7938b61ba127b16481
Adrian Conlon <adrian.conlon@gmail.com>
2015-11-08 20:37:46 +0000

    Mostly created from lein new app...
    Signed-off-by: Adrian Conlon <adrian.conlon@gmail.com>

---
+++ src/com/github/moleskicoder/clojure_euler.clj
@@ -0,0 +1,7 @@
+(ns com.github.moleskicoder.clojure-euler
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/com/github/moleskicoder/clojure_euler_test.clj
@@ -0,0 +1,7 @@
+(ns com.github.moleskicoder.clojure-euler-test
+  (:require [clojure.test :refer :all]
+            [com.github.moleskicoder.clojure-euler :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
